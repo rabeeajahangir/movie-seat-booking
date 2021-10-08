db0f106b86cb9b7c9f07930dec7a00b894424fe4
rabeea jahangir <rabeeajahangir@gmail.com>
2021-10-08 00:39:21 -0400

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,30 @@ const total = document.getElementById('total');
 const movieSelect = documet.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
+//Save Selected Movie Index
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 //Update total and count
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+//Cope selected seats into array //Map through the array //Return a new array indexes
+
+const seatsIndex = [...selectedSeats].map(function(seat) {
+    return [...seats].indexOf(seat)
+});
+
+localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+
+
+
+
+
+
 
     const selectedSeatsCount = selectedSeats.length;
 
@@ -20,8 +40,14 @@ function updateSelectedCount(){
 // Movie Selection
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
+
+
+
+
+
 // Seat  Selectio.
 
 
